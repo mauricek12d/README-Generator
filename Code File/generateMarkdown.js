7c5aaa644c2c4d6e4d39ca2f82b8abbc9d425ddd
Maurice Zuniga <mauricek12d@gmail.com>
2024-10-22 13:40:59 -0400

    This file includes the index.js, what version of inquirer being used, and the coding for the prompts which you will see when you generate the README file

---
+++ Code File/generateMarkdown.js
@@ -0,0 +1,53 @@
+// Function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
+  return `![License](https://img.shields.io/badge/license-${license}-blue)`;
+}
+
+// Function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+  return `## License\nThis project is licensed under the ${license} license.`;
+}
+
+// Function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, please Email or GitHub me at:
+- Email: (${data.email})
+- GitHub: [${data.github}](https://github.com/${data.github})`
+}
+
+export default generateMarkdown;
---
+++ Code File/index.js
@@ -0,0 +1,77 @@
+// Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from "./generateMarkdown.js";
+
+// Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address for questions:',
+    }
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Successfully created README!')
+    );
+}
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((responses) => {
+            const markdown = generateMarkdown(responses);
+            const fileName = `${responses.title.replace(/\s+/g, '_')}_README.md`;
+            writeToFile(fileName, markdown);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+// Function call to initialize app
+init();
